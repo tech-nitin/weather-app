066dfc6dfb0dac1cc35482b0e66ee5aed49ae95c
tech-nitin <technitin007@gmail.com>
2026-01-15 14:00:43 +0530

    Initial commit - Weather App

---
+++ script.js
@@ -0,0 +1,65 @@
+const apikey = "bdf6219843124b5fdaf60d0f72e689a3"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "img/images/clouds.png";
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "img/images/clear.png";
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "img/images/rain.png";
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "img/images/drizzle.png";
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "img/images/mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+
+
+
+
+}
+
+searchBtn.addEventListener("click", () => {
+  if (searchBox.value.trim() === "") {
+    alert("Please enter a city name");
+    return;
+  }
+  checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    if (searchBox.value.trim() === "") {
+      alert("Please enter a city name");
+      return;
+    }
+    checkWeather(searchBox.value);
+  }
+});
+
